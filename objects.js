402131d3ea84bf1ce7fba8b8abf7254dcd7f4dd8
Jason Decastro <jasonrdecastro@gmail.com>
2016-08-30 15:18:54 -0400

    finished javascript objects

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist = {1: 1}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
